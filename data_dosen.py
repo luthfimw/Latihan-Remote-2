06fe0518df889e1f33cbd53744d004c71c7fb1e5
Luthfi Mahendra W <lmahendraw@gmail.com>
2024-01-11 23:07:18 +0700

    telah melakukan penambahan list, menampilkan nomor data

--- data_dosen.py
+++ data_dosen.py
@@ -6,7 +6,7 @@ saya ingin mengumpulkan data nama, alamat, kota asal
 '''
 
 list_semua = []
-
+nomor_data = []
 print(f'{"="*20} SELAMAT DATANG  {"="*20}')
 while True:
     nama = input("Masukkan nama dosen lengkap  \t: ")
@@ -20,12 +20,30 @@ while True:
 
     data_kumpul = [nama,alamat,kota_asal]
     list_semua.append(data_kumpul)
-
-    pilihan = input('Apakah anda ingin memasukkan data dosen yang lainnya? ktik "iya" jika lanjut, ketik "tidak" jika sudah selesai input data : ')
+    no_sekarang = [len(list_semua)]
+    nomor_data.extend(no_sekarang)
+    
+    while True:
+        pilihan = input('Apakah anda ingin memasukkan data dosen yang lainnya? ktik "iya" jika lanjut, ketik "tidak" jika sudah selesai input data : ').lower()
+        print(f'Anda memiliki nomor kartu data diri yang telah anda inputkan : {len(list_semua)}')
+        if pilihan == 'tidak':
+            print('Nomor kartu yang anda miliki :', end='')
+            for i in nomor_data:
+                print(i,end=', ')
+            print()
+            print(f'{"="*20} TERIMAKASIH TELAH MENGGUNAKAN PROGRAM INI {"="*20}')
+            break
+        if pilihan == 'iya':
+            
+            break
+        else:
+            print('Mohon masukkan "iya" atau "tidak"')
+    if pilihan == 'iya':
+        continue
     if pilihan == 'tidak':
-        print(f'{"="*20} TERIMAKASIH TELAH MENGGUNAKAN PROGRAM INI {"="*20}')
         break
     
+
 #mencetak data 
 for i in list_semua:
     print(f'{"="*10} Berikut ini adalah tampilan data kalian {"="*10}')
@@ -33,4 +51,7 @@ for i in list_semua:
     print(f'Alamat \t : {i[1]}')
     print(f'Kota Asal: {i[2]}')
     print(f'{"="*30}')
+    
+#menghapus data tertentu
+
     
